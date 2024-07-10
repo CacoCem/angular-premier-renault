61b593c54e2cbef770c7f73cf687c4f14088697a
Cauan Cem <cacocembritosoares@gmail.com>
2024-07-10 15:33:05 -0300

    Componente Página Home criado.

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { MenuBarComponent } from '../../components/menu-bar/menu-bar.component';
+import { CarouselComponent } from '../../components/carousel/carousel.component';
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MenuBarComponent,CarouselComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  images = [
+    {
+      imgSrc:
+      'https://cdn.appdealersites.com.br/premier/julho-renault-ofertas/Kardian%20banner.webp',
+      imgAlt: 'imagem1'
+    },
+    {
+      imgSrc:
+      'https://cdn.appdealersites.com.br/premier/julho-renault-ofertas/Kwid%20Banner%20Final.webp',
+      imgAlt: 'imagem2'
+    },
+    {
+      imgSrc:
+      'https://cdn.appdealersites.com.br/premier/julho-renault-ofertas/WhatsApp%20Image%202024-07-09%20at%2014.08.12.webp',
+      imgAlt: 'imagem3'
+    },
+    {
+      imgSrc:
+      'https://cdn.appdealersites.com.br/premier/julho-renault-ofertas/Duster%20Intense%20Plus%201.6.webp',
+      imgAlt: 'imagem4'
+    }
+  ]
+}
